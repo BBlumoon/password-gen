c210554e8a4de6110b8f1f473dd0fe3bdd85bb0e
BBlumoon <127903224+BBlumoon@users.noreply.github.com>
2023-05-06 06:05:08 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 // Assignment code here
+var specialChar = '!@#$%^&*()'
+var numbers = '123456789'
+var lowerCase = 'abcdefghijklmnopqrstuvwxyz'
+var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+function generatePassword() {
+  var array = ''
+  var password = ''
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+  if(document.getElementById('upperCase').checked) {
+    array += upperCase
+  }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  if(document.getElementById('lowerCase').checked) {
+    array += lowerCase
+  }
 
-  passwordText.value = password;
+  if(document.getElementById('numbers').checked) {
+    array += numbers
+  }
 
+  if(document.getElementById('specialChar').checked) {
+    array += specialChar
+  }
+
+  if(array == '')
+  password = 'Must pick at least one character type'
+
+  else {
+    for (let i = 0; i < 10; i++) {
+      password += array[Math.floor(Math.random() * array.length)]
+    }
+  }
+document.getElementById('password').value = password;
+console.log('it works')
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
